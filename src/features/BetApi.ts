9c7c87756c98cc14d696974b181d36b04ce699a7
mdebowska <malgorzatadebowska96@gmail.com>
2022-09-11 22:21:46 +0200

    🎨 prettier formating

--- src/features/BetApi.ts
+++ src/features/BetApi.ts
@@ -1,12 +1,22 @@
-import axios from "axios";
-import BetEntry from "../models/BetEntry";
-const path = "http://localhost:8000";
+import axios from 'axios'
+import BetEntry from '../models/BetEntry'
+const path = 'http://localhost:8000'
 
 export default class BetApi {
-
   async getAllBets(): Promise<BetEntry[]> {
     const result = await axios.get(`${path}/api/bet`)
-    return result.data.map((data: any) => new BetEntry(data.id, data.title, data.description, data.option1, data.option2, data.isFinished, data.winner))
+    return result.data.map(
+      (data: any) =>
+        new BetEntry(
+          data.id,
+          data.title,
+          data.description,
+          data.option1,
+          data.option2,
+          data.isFinished,
+          data.winner
+        )
+    )
   }
 
   async getBetById(betId: number) {
@@ -14,8 +24,8 @@ export default class BetApi {
   }
 
   async addNewBet(request: BetEntry): Promise<BetEntry> {
-    const { data } = await axios.post(`${path}/api/bet`, request);
-    return new BetEntry(data.id, data.title, "", data.option1, data.option2);
+    const { data } = await axios.post(`${path}/api/bet`, request)
+    return new BetEntry(data.id, data.title, '', data.option1, data.option2)
   }
 
   async updateBetById(bet: BetEntry) {
