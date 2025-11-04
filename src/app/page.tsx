17cf465f48c0bd00b2b79d0d8b63c57233c2c728
Radek Jajko <radek235s@gmail.com>
2025-11-04 18:51:46 +0100

    ⚰️ #41 Delete unused code

--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function HomePage() {
-    return <div>
-        <h1>Bet Keeper</h1>
-    </div>
-}
--- src/views/dashboard/components/widgets/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as DashboardCreateBetWidget } from './DashboardCreateBetCard'
-export { default as MonthlySummary } from './MonthlySummary'
-export { default as OverallSummary } from './OverallSummary'
