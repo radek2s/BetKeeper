aaef5a571ffe4a0d96e43bbc220bae2479462cb9
Radek Jajko <radek235s@gmail.com>
2025-11-05 20:19:55 +0100

    ⚙️ #41 Fix configuration

--- application/next.config.js
+++ application/next.config.js
@@ -1,5 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins } = require("@nx/next");
+const { loadEnvConfig } = require("@next/env");
+
+loadEnvConfig(process.cwd());
+
+const DATABASE_URL = process.env.DB_PRISMA_URL;
+const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION;
+console.log(`*** BetKeeper v${APP_VERSION} ***`);
+console.log(`Using SQLite file: ${DATABASE_URL}\n`);
 
 const nextConfig = {
   // Use this to set Nx-specific options
--- application/src/constants.ts
+++ application/src/constants.ts
@@ -1,2 +1,1 @@
-// export const ACTIVE_USER_ID = "d1d94dd0-6c44-4d34-ab11-cc1caca6acc1";
-export const ACTIVE_USER_ID = "00000000-0000-0000-0000-000011110000";
+export const ACTIVE_USER_ID = process.env.DEV_USER_ID;
