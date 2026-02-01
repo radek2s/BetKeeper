3c8bc7c7c3930e043cf404329b7fedb939b83130
Radek Jajko <radek235s@gmail.com>
2026-02-01 14:38:16 +0100

    Prepre Prisma for Supabase

--- application/src/prisma/postgres/index.ts
+++ application/src/prisma/postgres/index.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@database/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 export function getPostgresClient() {
-  const connectionString = `${process.env.DATABASE_URL}`;
+  const connectionString = `${process.env.DATABASE_URL_PG}`;
 
   const adapter = new PrismaPg({ url: connectionString });
   const prismaClient = new PrismaClient({ adapter });
