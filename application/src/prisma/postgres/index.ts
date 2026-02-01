a1c8e8a18da6d0e2ddcb759780f3570eb5b63f08
Radek Jajko <radek235s@gmail.com>
2026-02-01 20:58:18 +0100

    Fixing postgres connection

--- application/src/prisma/postgres/index.ts
+++ application/src/prisma/postgres/index.ts
@@ -1,11 +1,18 @@
-import "dotenv/config";
 import { PrismaClient } from "@database/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import "dotenv/config";
 
 export function getPostgresClient() {
   const connectionString = `${process.env.DATABASE_URL_PG}`;
 
-  const adapter = new PrismaPg({ url: connectionString });
+  const pool = new Pool({
+    connectionString: connectionString,
+    ssl: { rejectUnauthorized: false },
+    max: 10,
+  });
+
+  const adapter = new PrismaPg(pool);
   const prismaClient = new PrismaClient({ adapter });
 
   return prismaClient;
