b2c3014e60e936db9e83a7ac2046533e99ae2a99
Radek Jajko <radek235s@gmail.com>
2026-03-29 21:24:03 +0200

    #48 Fix E2E test execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -41,6 +41,9 @@ export default defineConfig({
 
   webServer: {
     command: "npm run e2e:dev",
+    env: {
+      DATABASE_URL: "file:./database/data/betKeeperE2E.db",
+    },
     url: "http://localhost:3000",
     reuseExistingServer: !process.env["CI"],
     timeout: 120000,
